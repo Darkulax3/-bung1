9c111748944f8fbb294c75581f15381869e2b77e
Darkulax3 <niklas.geist@gmx.de>
2024-04-26 10:40:48 +0200

    Einstieg in Javascript

---
+++ Javascriptuebung.js
@@ -0,0 +1,19 @@
+let str = "Hallo Welt";
+let mybool;
+//Hiermit prüft man den Typ einer Variablen: true, '', str, number
+//const hallo=typeof()
+
+for (let i = 0; i < str.length; i++) {
+   
+    if (str[i]==' ') {
+        console.log ('Hier gibt es Geister')
+        
+} 
+    
+}
+
+let hallo= Number(prompt("Gebe eine Zahl ein:"))
+
+// hallo = 5 ? console.log("Du hast eine 5") : console.log("Nicht meine zahl") 
+
+if (hallo == )
